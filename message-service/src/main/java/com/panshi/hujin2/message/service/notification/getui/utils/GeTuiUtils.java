c1797f77d0de286bb1695830ccd7d55be02bdcea
wangxiliang <wangxiliang1991@outlook.com>
2019-07-16 14:50:44 +0800

    新增马甲信息

--- message-service/src/main/java/com/panshi/hujin2/message/service/notification/getui/utils/GeTuiUtils.java
+++ message-service/src/main/java/com/panshi/hujin2/message/service/notification/getui/utils/GeTuiUtils.java
@@ -119,6 +119,11 @@ public class GeTuiUtils {
                 infoBO.setAppKey(GeTuiAppConfigEnum.VI_CASH_CAT_APP_KEY.getValue());
                 infoBO.setMasterSecret(GeTuiAppConfigEnum.VI_CASH_CAT_MASTER_SECRET.getValue());
                 break;
+            case VI_IN_CASHCAT:
+                infoBO.setAppId(GeTuiAppConfigEnum.VI_IN_CASHCAT_APP_ID.getValue());
+                infoBO.setAppKey(GeTuiAppConfigEnum.VI_IN_CASHCAT_APP_KEY.getValue());
+                infoBO.setMasterSecret(GeTuiAppConfigEnum.VI_IN_CASHCAT_MASTER_SECRET.getValue());
+                break;
             default:
                 //参数错误异常
                 System.err.println(" ======获取个推推送key错误======= ");
